cc7d5455b5064d7452d829f0741fd3851be4c738
marsoln <marsoln1988@gmail.com>
2018-01-21 18:10:00 +0800

    get balance task parallel limitation

--- tasks/getBalance.js
+++ tasks/getBalance.js
@@ -1,18 +1,33 @@
 import web3 from '../framework/web3'
+import { TaskCapsule, ParallelQueue } from '../utils/task'
+
+let taskQueue = new ParallelQueue({
+  limit: 3,
+  onFinished: () => {
+    console.log('finished!')
+    process.exit(0)
+  },
+})
 
 async function getBalance() {
   let connect = await web3.onWs
   let listAccounts = await connect.eth.getAccounts()
-  listAccounts.forEach(async (address) => {
-    await connect.eth.getBalance(address)
-      .then((amount) => {
-        console.log(`钱包地址 ${address} eth 余额 ${connect.eth.extend.utils.fromWei(amount, 'ether')}`)
+  listAccounts.forEach((address) => {
+    taskQueue.add(new TaskCapsule(() =>
+      new Promise((resolve, reject) => {
+        connect.eth.getBalance(address)
+          .then((amount) => {
+            console.log(`钱包地址 ${address} eth 余额 ${connect.eth.extend.utils.fromWei(amount, 'ether')}`)
+            resolve()
+          })
+          .catch((ex) => {
+            console.error(`get address eth balance failded: ${address}`)
+            reject(ex)
+          })
       })
-      .catch((ex) => {
-        console.error(`get address eth balance failded: ${address}`)
-        process.exit(-1)
-      })
+    ))
   })
+  taskQueue.consume()
 }
 
 export default getBalance
