5488f9f0210e6edacc7ee5d938e1dd7b47fefd4d
marsoln <marsoln1988@gmail.com>
2018-04-23 20:15:08 +0800

    add sync wallet task

--- src/server/jobs/index.js
+++ src/server/jobs/index.js
@@ -1,22 +1,26 @@
 import agendaClient from '../../framework/jobsManager'
 import scanBlocksJob from './scanBlocks'
 import syncTransactionState from './syncTxState'
+import syncWallet from './syncWallet'
 // import scanBlocksForContractJob from './scanBlocksForContract'
 
 const TASKS = {
   scanBlocks: 'scan blocks',
   syncTxState: 'sync transaction state',
+  syncWallet: 'sync wallet info',
   // scanBlocksForContract: 'scan blocks for contract',
 }
 
 agendaClient.define(TASKS.scanBlocks, scanBlocksJob)
 agendaClient.define(TASKS.syncTxState, syncTransactionState)
+agendaClient.define(TASKS.syncWallet, syncWallet)
 // agendaClient.define(TASKS.scanBlocksForContract, scanBlocksForContractJob)
 
 export default () => {
   agendaClient.on('ready', () => {
     agendaClient.every('2 minutes', TASKS.scanBlocks).unique({ scanBlock: true })
     agendaClient.every('5 minutes', TASKS.syncTxState).unique({ syncTxState: true })
+    agendaClient.every('5 minutes', TASKS.syncWallet).unique({ syncWallet: true })
     agendaClient.start()
   }).on('error', (ex) => {
     console.log(ex)
---
+++ src/server/jobs/syncWallet.js
@@ -0,0 +1,47 @@
+import { TaskCapsule, ParallelQueue } from 'async-task-manager'
+
+import { postBalances } from '../../apis/phpApis'
+import { connect } from '../../framework/web3'
+
+export default async function syncWallet(job, done) {
+  let list = await connect.eth.getAccounts()
+  let balanceArr = []
+  let total = 0
+
+  const taskQueue = new ParallelQueue({ limit: 20, toleration: 0 })
+
+  if (list.length > 0) {
+    for (let i = 0; i < list.length; i += 1) {
+      taskQueue.add(
+        new TaskCapsule(() =>
+          new Promise(async (resolve, reject) => {
+            let address = list[i]
+            let amount_wei = await connect.eth.getBalance(address).catch(reject)
+            let amount = connect.eth.extend.utils.fromWei(amount_wei, 'ether')
+            total += +amount
+            balanceArr.push({ address, amount })
+            resolve('succ')
+          })
+        )
+      )
+    }
+
+    taskQueue
+      .consume()
+      .then(() => {
+        console.log(`总币量${total} 总地址数${balanceArr.length}`)
+        postBalances(balanceArr)
+          .then((res) => {
+            console.log(`钱包信息查询完毕，提交到 php 服务器: ${JSON.stringify(res, null, 4)}`)
+            done()
+          })
+          .catch((err) => {
+            console.log(`定时任务队列执行错误: ${err.message}`)
+            done()
+          })
+      })
+      .catch(() => { done() })
+  } else {
+    throw new Error('地址数组为空')
+  }
+}
