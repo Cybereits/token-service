20b89dd4da3de2db65ee7b2c3fcd8a1a6c18eff4
marsoln <marsoln1988@gmail.com>
2018-04-23 15:18:15 +0800

    fix urlHelper issue

--- src/apis/urlHelper.js
+++ src/apis/urlHelper.js
@@ -1,16 +1,22 @@
 const matchReg = /([?&][^#?&=]*=?[^#?&]*)+/ig
 const anchorReg = /#[^?]*/ig
 
+/**
+ * 获取链接地址中的参数
+ * @param {string} url 链接地址
+ */
 export function getParams(url) {
   let anchorMatches = url.match(anchorReg)
   let temp = url
   let matches = temp.match(matchReg)
   let matchStr = matches ? matches[0] : undefined
   let params = {}
-  let action = temp.substr(0,
+  let action = temp.substr(
+    0,
     temp.length -
     (matchStr ? matchStr.length : 0) -
-    (anchorMatches ? anchorMatches[0].length : 0))
+    (anchorMatches ? anchorMatches[0].length : 0),
+  )
 
   if (matchStr) {
     // 拆出参数数组
@@ -29,7 +35,13 @@ export function getParams(url) {
   }
 }
 
-export function concatUrl(url, params, needEncode = true) {
+/**
+ * 拼接参数生成 url
+ * @param {string} url 链接地址
+ * @param {object} params 参数对象
+ * @param {boolean} need_encode 是否需要 encode
+ */
+export function concatUrl(url, params, need_encode = true) {
   // 参数不为对象 或 参数为空对象 返回原字符串
   if (!(params instanceof Object) || Object.keys(params).length === 0) {
     return url
@@ -48,7 +60,7 @@ export function concatUrl(url, params, needEncode = true) {
       if (newParams[n] instanceof Array) {
         let arr = newParams[n]
         arr.forEach((v) => {
-          p += `${n}=${v}&`
+          p += `${n}[]=${v}&`
         })
       } else {
         p += `${n}=${newParams[n]}&`
@@ -58,7 +70,12 @@ export function concatUrl(url, params, needEncode = true) {
   }, `${action}?`)
 
   let ret = temp.substr(0, temp.length - 1) + (anchorMatches ? anchorMatches[0] : '')
-  if (needEncode) { return encodeURI(ret) } else { return ret }
+
+  if (need_encode) {
+    return encodeURI(ret)
+  }
+
+  return ret
 }
 
 export default {
