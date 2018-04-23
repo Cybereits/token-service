6042bdcd4441c314c55bccaf4baf92c185f86e7c
marsoln <marsoln1988@gmail.com>
2018-04-23 19:15:37 +0800

    restore http actions

--- src/server/routes.js
+++ src/server/routes.js
@@ -2,11 +2,16 @@ import Router from 'koa-router'
 import bodyparser from 'koa-bodyparser'
 import { graphqlKoa, graphiqlKoa } from 'apollo-server-koa'
 import GraphqlSchema from './graphql'
+import { create, list } from './actions/account'
 
 const router = new Router()
 
 router.post('/graphql', bodyparser({ enableTypes: ['json', 'form', 'text'] }), graphqlKoa({ schema: GraphqlSchema }))
 router.get('/graphql', graphqlKoa({ schema: GraphqlSchema, cacheControl: true }))
 router.get('/data', graphiqlKoa({ endpointURL: '/graphql' }))
+
+// 钱包相关
+router.post('/account/create', create)
+router.get('/account/list', list)
 
 export default router
