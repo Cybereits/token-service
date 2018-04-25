a0f4ae6973539507d72aa9d1b12d2fa653462b25
marsoln <marsoln1988@gmail.com>
2018-04-25 17:47:51 +0800

    remove sync wallet task

--- src/server/jobs/index.js
+++ src/server/jobs/index.js
@@ -1,7 +1,7 @@
 import agendaClient from '../../framework/jobsManager'
 import scanBlocksJob from './scanBlocks'
 import syncTransactionState from './syncTxState'
-import syncWallet from './syncWallet'
+// import syncWallet from './syncWallet'
 // import scanBlocksForContractJob from './scanBlocksForContract'
 
 const TASKS = {
@@ -13,14 +13,14 @@ const TASKS = {
 
 agendaClient.define(TASKS.scanBlocks, scanBlocksJob)
 agendaClient.define(TASKS.syncTxState, syncTransactionState)
-agendaClient.define(TASKS.syncWallet, syncWallet)
+// agendaClient.define(TASKS.syncWallet, syncWallet)
 // agendaClient.define(TASKS.scanBlocksForContract, scanBlocksForContractJob)
 
 export default () => {
   agendaClient.on('ready', () => {
     agendaClient.every('10 minutes', TASKS.scanBlocks).unique({ scanBlock: true })
     agendaClient.every('20 minutes', TASKS.syncTxState).unique({ syncTxState: true })
-    agendaClient.every('12 hours', TASKS.syncWallet).unique({ syncWallet: true })
+    // agendaClient.every('12 hours', TASKS.syncWallet).unique({ syncWallet: true })
     agendaClient.start()
   }).on('error', (ex) => {
     console.log(ex)
