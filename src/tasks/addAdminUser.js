38b2565907c5f16d3696f87c7929b30d0478bbb6
zhanglei <zhanglei.tsunami@gmail.com>
2018-06-06 13:56:33 +0800

    fix issues

--- src/tasks/addAdminUser.js
+++ src/tasks/addAdminUser.js
@@ -1,26 +1,23 @@
-import { AdminModel } from '../core/schemas';
+import { AdminModel } from '../core/schemas'
 
-export default (username, password, validPassword, role = 2) => {
-  if (password != validPassword) {
+export default async (username, password, validPassword, role = 2) => {
+  if (password !== validPassword) {
     console.log('两次输入密码不一致')
     process.exit()
-    return
   }
 
-  AdminModel.findOne({ username })
-    .then(adminUser => {
-      if (adminUser) {
-        console.log('用户已存在');
-        process.exit()
-        return
-      }
-      let admin = new AdminModel({username, password, role })
-      admin.save()
-      .then(newAdmin => {
-        process.exit()
-      })
-      .catch(err => {
-        process.exit()
-      })
-    });
+  let admin = await AdminModel.findOne({ username })
+  if (admin) {
+    console.log('用户已存在')
+    process.exit()
+  }
+  admin = new AdminModel({ username, password, role })
+  admin.save()
+  .then((newAdmin) => {
+    process.exit()
+  })
+  .catch((err) => {
+    console.error(err)
+    process.exit()
+  })
 }
