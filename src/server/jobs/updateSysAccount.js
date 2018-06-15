2389f9102773860f6dfc4202375765cb06564b1d
marsoln <marsoln1988@gmail.com>
2018-06-15 14:39:24 +0800

    make update sys account as a schedule job again..

--- src/server/jobs/updateSysAccount.js
+++ src/server/jobs/updateSysAccount.js
@@ -3,19 +3,40 @@ import { TaskCapsule, ParallelQueue } from 'async-task-manager'
 import { getAllAccounts, updateBalanceOfAccount } from '../../core/scenes/account'
 import { getAllTokenContracts } from '../../core/scenes/contract'
 
-getAllAccounts().then(async (accounts) => {
-  const taskQueue = new ParallelQueue({ limit: 20 })
-  const tokenContractNames = await getAllTokenContracts().then(res => res.map(({ name }) => name))
-  accounts.forEach((address) => {
-    taskQueue.add(new TaskCapsule(() => updateBalanceOfAccount(address)))
-    tokenContractNames.forEach((contractName) => {
-      taskQueue.add(new TaskCapsule(() => updateBalanceOfAccount(address, contractName)))
-    })
-  })
+let executable = true
 
-  taskQueue.consume().then(() => {
-    console.info('系统账户余额信息更新完成')
-  }).catch((ex) => {
-    console.error(ex.message)
-  })
-})
+export default function (job, done) {
+  if (!executable) {
+    console.info('尚有未完成交易状态同步任务...')
+    done()
+  }
+
+  executable = false
+
+  getAllAccounts()
+    .then(async (accounts) => {
+      const taskQueue = new ParallelQueue({ limit: 20 })
+      const tokenContractNames = await getAllTokenContracts().then(res => res.map(({ name }) => name))
+      accounts.forEach((address) => {
+        taskQueue.add(new TaskCapsule(() => updateBalanceOfAccount(address)))
+        tokenContractNames.forEach((contractName) => {
+          taskQueue.add(new TaskCapsule(() => updateBalanceOfAccount(address, contractName)))
+        })
+      })
+
+      taskQueue.consume().then(() => {
+        executable = true
+        done()
+        console.info('系统账户余额信息更新完成')
+      }).catch((ex) => {
+        executable = true
+        console.error(ex.message)
+        done()
+      })
+    })
+    .catch((ex) => {
+      executable = true
+      console.error(ex.message)
+      done()
+    })
+}
