07ae596f8d7c46f9a0094094592d5d25317cca0b
hhw852791435@163.com <hhw852791435@163.com>
2018-06-21 16:17:59 +0800

    SearchContract changes

--- app/src/models/contract.js
+++ app/src/models/contract.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import {
   queryAllContract,
   addERC20ContractMeta,
@@ -24,10 +25,34 @@ export default {
     },
     *queryAllContract({ params }, { call, put }) {
       const response = yield call(queryAllContract, params);
-      yield put({
-        type: 'save',
-        payload: response,
-      });
+      // console.log(response)
+      const data = {};
+      if (response) {
+        data.list = response.data.queryAllContract.map((item, index) => {
+          const newItem = { ...item };
+          for (const key in newItem) {
+            if (key === 'isERC20') {
+              newItem[key] = newItem[key] === true ? '是' : '否';
+            }
+          }
+          return {
+            ...newItem,
+            key: index,
+            createAt: item.createAt === '' || moment(item.createAt).format('YYYY-MM-DD hh:mm:ss'),
+          };
+        });
+        data.pagination = {
+          total: data.list.length,
+          current: 1,
+          pageSize: data.list.length,
+          pageCount: 1,
+        };
+        // console.log(data)
+        yield put({
+          type: 'save',
+          payload: data,
+        });
+      }
     },
     *deployCREContract({ params, callback }, { call }) {
       const response = yield call(deployCREContract, params);
