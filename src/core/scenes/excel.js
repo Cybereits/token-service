a37a49fec5261f0222b7785d1a2bbfb2ba2fc4ad
marsoln <marsoln1988@gmail.com>
2018-08-20 16:00:36 +0800

    little change

--- src/core/scenes/excel.js
+++ src/core/scenes/excel.js
@@ -1,23 +1,16 @@
 import { exec } from 'child_process'
-// import fs from 'fs'
 import path from 'path'
-import crypto from 'crypto'
 
 const exportFileDir = path.resolve(__dirname, '../../static/')
-const ITERATIONS = 1000
-const ALGORITHM = 'sha256'
-
-function getHash(str, length = 10) {
-  return crypto.pbkdf2Sync(str, Date.now().toString(), ITERATIONS, length, ALGORITHM).toString('hex')
-}
 
 /**
  * 导出数据库实体到 Csv 文件
  * @param {String} collection_name 表名称
  * @param {Array<String>} fields 字段数组
+ * @param {String} filename 导出的文件名
  */
-export function exportToCsv(collection_name, fields) {
-  let output_filename = `${getHash(collection_name)}.csv`
+export function exportToCsv(collection_name, fields, filename) {
+  let output_filename = `${filename || collection_name}.csv`
   let output_filepath = path.resolve(exportFileDir, `${output_filename}`)
 
   let command = `mongoexport --db ethService --collection ${collection_name} --out ${output_filepath} --type csv --fields ${fields.join(',')}`
@@ -34,5 +27,5 @@ export function exportToCsv(collection_name, fields) {
 }
 
 export function exportAccountInfo() {
-  return exportToCsv('ethaccounts', ['account', 'balances.eth', 'balances.cre'])
+  return exportToCsv('ethaccounts', ['account', 'balances.eth', 'balances.cre', 'comment'], '账户信息')
 }
